c7dfe5e66901b5c1b9ab8bd0aafc18d03a5d4c77
windiartikah <windiartikah25@gmail.com>
2022-03-18 09:22:51 +0700

    array tipe primitive

---
+++ src/main/kotlin/array tipe primitive.kt
@@ -0,0 +1,23 @@
+fun main() {
+    val x: IntArray = intArrayOf(1, 2, 3)
+    x[0] = x[1] + x[2]
+    x.forEach { print(it.toString() + ' ')}
+    print('\n')
+
+//Array dari int ukuran 5 dengan nilai [0, 0, 0, 0, 0]
+    val byteArray = ByteArray(5)
+    byteArray.forEach {print(it.toString() + ' ')}
+    print('\n')
+
+//misalnya inisialisasi nilai-nilai dalam array dengan konstanta
+//Array int ukuran 5 dengan nilai [42, 42, 42, 42, 42]
+    val shortArray = ShortArray(5) {42}
+    shortArray.forEach { print(it.toString() + ' ')}
+    print('\n')
+
+// misalnya inisialisasi nilai-nilai dalam array menggunakan lambda
+// Array int ukuran 5 dengan nilai [0, 1, 2, 3, 4] (nilai diinisialisasi ke nilai indeksnya)
+    var intArray = IntArray(5) {it * 1}
+    intArray.forEach { print(it.toString() + ' ') }
+    print('\n')
+}
