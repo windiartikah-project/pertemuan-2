f17b917e3beabad3e3d36934abcd1ae6ac0a35ad
windiartikah <windiartikah25@gmail.com>
2022-03-18 09:40:41 +0700

    collection

---
+++ src/main/kotlin/collection.kt
@@ -0,0 +1,11 @@
+fun printAll(strings: Collection<String>) {
+    for(s in strings) print("$s ")
+    println()
+}
+fun main(args: Array<String>) {
+    val stringList = listOf("one", "two", "one")
+    printAll(stringList)
+    val stringSet = setOf("one", "two", "three")
+    printAll(stringSet)
+    //biasanya berisi sejumlah objek (mungkin berjumlah 0) yang memiliki tipe yang sama.
+}
